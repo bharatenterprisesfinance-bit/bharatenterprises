ebbb77cf489f54b179605cb774a400e77d13822c
Avi@2000005 <chateavinash123@gmail.com>
2026-09-11 01:51:01 +0530

    first commit

--- src/types.ts
+++ src/types.ts
@@ -82,5 +82,5 @@ export const COMPANY_DETAILS = {
   phone: "+91 91-302-302-33",
   cleanPhone: "919130230233",
   displayPhone: "+91 91302 30233",
-  email: "Bharatenterprisesfinance@gmail.com",
+  email: "bharatenterprisesfinance@gmail.com",
 };
